f115417162441868cd887a3741b07ad58957f072
Sylvain <sylvain.lapeyrade@hotmail.fr>
2019-11-26 17:16:08 +0100

    update

--- TDDD07_lab_release_2019-09-13/robot_agent/src/scheduler.c
+++ TDDD07_lab_release_2019-09-13/robot_agent/src/scheduler.c
@@ -33,7 +33,7 @@ volatile sig_atomic_t stop;
 scheduler_t *scheduler_init(void)
 {
 	// Allocate memory for Scheduler structure
-	scheduler_t *ces = (scheduler_t *) malloc(sizeof(scheduler_t));
+	scheduler_t *ces = (scheduler_t *)malloc(sizeof(scheduler_t));
 
 	return ces;
 }
@@ -77,7 +77,7 @@ void scheduler_wait_for_timer(scheduler_t *ces)
 	timelib_timer_add_ms(&ces->tv_cycle, ces->minor);
 
 	// Check for overrun and execute sleep only if there is no
-	if(sleep_time > 0)
+	if (sleep_time > 0)
 	{
 		// Go to sleep (multipy with 1000 to get miliseconds)
 		usleep(sleep_time);
@@ -92,44 +92,45 @@ void scheduler_wait_for_timer(scheduler_t *ces)
  */
 void scheduler_exec_task(scheduler_t *ces, int task_id)
 {
-	switch(task_id)
+	switch (task_id)
 	{
 	// Mission
-	case s_TASK_MISSION_ID :
+	case s_TASK_MISSION_ID:
 		task_mission();
 		break;
 	// Navigate
-	case s_TASK_NAVIGATE_ID :
+	case s_TASK_NAVIGATE_ID:
 		task_navigate();
 		break;
 	// Control
-	case s_TASK_CONTROL_ID :
+	case s_TASK_CONTROL_ID:
 		task_control();
 		break;
 	// Refine
-	case s_TASK_REFINE_ID :
+	case s_TASK_REFINE_ID:
 		task_refine();
 		break;
 	// Report
-	case s_TASK_REPORT_ID :
+	case s_TASK_REPORT_ID:
 		task_report();
 		break;
 	// Communicate
-	case s_TASK_COMMUNICATE_ID :
+	case s_TASK_COMMUNICATE_ID:
 		task_communicate();
 		break;
 	// Collision detection
-	case s_TASK_AVOID_ID :
+	case s_TASK_AVOID_ID:
 		task_avoid();
 		break;
 	// Other
-	default :
+	default:
 		// Do nothing
 		break;
 	}
 }
 
-void signal_handeling(){
+void signal_handeling()
+{
 	stop = 1;
 	printf("Interrupt Signal sent.\n");
 }
@@ -145,7 +146,6 @@ void scheduler_run(scheduler_t *ces)
 	signal(SIGINT, signal_handeling);
 	struct timeval start;
 
-
 	/* --- Set minor cycle period --- */
 	ces->minor = 1 * 100;
 
@@ -154,42 +154,40 @@ void scheduler_run(scheduler_t *ces)
 
 	// Start scheduler
 	scheduler_start(ces);
-	
-	while(!stop){
-	timelib_timer_set(&start);
 
-	scheduler_exec_task(ces, s_TASK_MISSION_ID);
-	double exec_time1 = timelib_timer_get(start);
+	while (!stop)
+	{
+		timelib_timer_set(&start);
 
-	scheduler_exec_task(ces, s_TASK_NAVIGATE_ID);
-	double exec_time2 = timelib_timer_get(start);
+		scheduler_exec_task(ces, s_TASK_MISSION_ID);
+		double exec_time1 = timelib_timer_get(start);
 
-	scheduler_exec_task(ces, s_TASK_CONTROL_ID);
-	double exec_time3 = timelib_timer_get(start);
-		
-	scheduler_exec_task(ces, s_TASK_REPORT_ID);
-	double exec_time4 = timelib_timer_get(start);
+		scheduler_exec_task(ces, s_TASK_NAVIGATE_ID);
+		double exec_time2 = timelib_timer_get(start);
 
-	scheduler_exec_task(ces, s_TASK_REFINE_ID);
-	double exec_time5 = timelib_timer_get(start);
+		scheduler_exec_task(ces, s_TASK_CONTROL_ID);
+		double exec_time3 = timelib_timer_get(start);
 
-	scheduler_exec_task(ces, s_TASK_AVOID_ID);
-	double exec_time6 = timelib_timer_get(start);
+		scheduler_exec_task(ces, s_TASK_REPORT_ID);
+		double exec_time4 = timelib_timer_get(start);
 
-	scheduler_exec_task(ces, s_TASK_COMMUNICATE_ID);
-	double exec_time7 = timelib_timer_get(start);
+		scheduler_exec_task(ces, s_TASK_REFINE_ID);
+		double exec_time5 = timelib_timer_get(start);
 
+		scheduler_exec_task(ces, s_TASK_AVOID_ID);
+		double exec_time6 = timelib_timer_get(start);
 
-	
-	printf("exec_time1 %f ms.\n", exec_time1);
-	printf("exec_time2 %f ms.\n", exec_time2);
-	printf("exec_time3 %f ms.\n", exec_time3);
-	printf("exec_time4 %f ms.\n", exec_time4);
-	printf("exec_time5 %f ms.\n", exec_time5);
-	printf("exec_time6 %f ms.\n", exec_time6);
-	printf("exec_time7 %f ms.\n\n", exec_time7);
-		
-	//scheduler_wait_for_timer(ces);
+		scheduler_exec_task(ces, s_TASK_COMMUNICATE_ID);
+		double exec_time7 = timelib_timer_get(start);
+
+		printf("exec_time1 %f ms.\n", exec_time1);
+		printf("exec_time2 %f ms.\n", exec_time2);
+		printf("exec_time3 %f ms.\n", exec_time3);
+		printf("exec_time4 %f ms.\n", exec_time4);
+		printf("exec_time5 %f ms.\n", exec_time5);
+		printf("exec_time6 %f ms.\n", exec_time6);
+		printf("exec_time7 %f ms.\n\n", exec_time7);
+
+		//scheduler_wait_for_timer(ces);
 	}
-	
 }
